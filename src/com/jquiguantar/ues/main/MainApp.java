67d1ba68646ed0c25f43a330165a0d7bbe10e5a0
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-04-25 08:13:06 -0500

    Revision y uso de ramas en git

--- src/com/jquiguantar/ues/main/MainApp.java
+++ src/com/jquiguantar/ues/main/MainApp.java
@@ -1,5 +1,6 @@
 package com.jquiguantar.ues.main;
 
+//Revision de uso y manejo de ramas en git
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Iniciando Sistema De Emergencias Urbanas...");
