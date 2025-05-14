59d4a96a046c0480707e0bd037ba00bfc278a5b4
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-05-14 15:35:21 -0500

    Hacemos una refactorizacion general

--- src/com/jquiguantar/ues/model/emergencies/TipoEmergencia.java
+++ src/com/jquiguantar/ues/model/emergencies/TipoEmergencia.java
@@ -1,11 +1,5 @@
 package com.jquiguantar.ues.model.emergencies;
 
-import com.jquiguantar.ues.model.resources.TipoRecurso;
-import java.util.Map;
-
-public interface TipoEmergencia {
-    // Devuelve el nombre descriptivo del tipo de emergencia
-    String getNombre();
-
-    Map<TipoRecurso, Integer> calcularRecursosInicialesNecesarios(NivelGravedad gravedad);
+public enum TipoEmergencia {
+    INCENDIO, ACCIDENTE_VEHICULAR, ROBO
 }
